41c44f86bb3095e215f9a502775782e63e7b45dd
sjrdc <scrijns@gmail.com>
2025-05-20 12:15:56 +0200

    Randomly update player coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,47 @@
 #include <QtWidgets/QApplication>
+#include <QTimer>
+#include <QRandomGenerator>
 #include "mainwindow.h"
+
+#include <array>
+
+namespace
+{
+    void generate_coordinates(auto& generator, auto highest, auto& coordinates)
+    {
+            for (auto& point : coordinates)
+            {
+                point = QPoint(generator.bounded(highest.width()), generator.bounded(highest.height()));
+            }
+    }
+
+    auto get_player_coordinates(auto& generator, auto highest)
+    {
+            pong::players players;
+            generate_coordinates(generator, highest, players.left_player);
+            generate_coordinates(generator, highest, players.right_player);
+            return players;
+    }
+
+    void run_updates(pong::mainwindow& w, QTimer& timer, QRandomGenerator& generator)
+    {
+        auto update_players = [&generator, &w]()
+        {
+            w.update(get_player_coordinates(generator, w.size()));
+        };
+        QObject::connect(&timer, &QTimer::timeout, update_players);
+        timer.start(500);
+    }
+}
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     
     pong::mainwindow w;
+    QTimer timer;
+    QRandomGenerator generator;
+    run_updates(w, timer, generator);
 
     return QApplication::exec();
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,15 +3,26 @@
 #include <QHBoxLayout>
 #include <QScreen>
 #include <QGuiApplication>
-
+#include <iostream>
 namespace
 {
     constexpr auto circle_color = Qt::white;
     constexpr auto background_color = Qt::black;
-    constexpr auto left_rectangle_color = Qt::red;
+    constexpr auto left_rectangle_color = Qt::green;
     constexpr auto right_rectangle_color = Qt::blue;
     constexpr auto stroke = 15.;
     constexpr auto border = 12.;
+    constexpr auto diameter = 35.;
+    constexpr auto radius_offset = QPointF(-0.5*diameter, -0.5*diameter);
+
+    void update_player_positions(const auto& positions, auto& graphics)
+    {
+        assert(positions.size() == graphics.size());
+        for (auto i = 0ul; i < positions.size(); ++i)
+        {
+            graphics[i]->setPos(positions[i] + radius_offset);
+        }
+    }
 }
 
 namespace pong
@@ -27,13 +38,15 @@ namespace pong
         setLayout(layout);
         view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
         view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+        this->setStyleSheet("QWidget { background-color: \"black\"; border-width: 0px; border: 0px; margin: 0px; }");
         showFullScreen();
         populate_scene();
     }
 
-    void update(const players& p)
+    void mainwindow::update(const players& p)
     {
-
+        update_player_positions(p.right_player, right_player_graphics);
+        update_player_positions(p.left_player, left_player_graphics);
     }
 
     void mainwindow::populate_scene()
@@ -62,7 +75,6 @@ namespace pong
         const auto make_ellipse = [this](auto& ptr) 
         {
             constexpr auto x = 0., y = 0.;
-            constexpr auto diameter = 35;
             ptr = std::make_unique<QGraphicsEllipseItem>(x, y, diameter, diameter); 
             ptr->setBrush(circle_color);
             scene.addItem(ptr.get());
